1fbb95d74ff37c035df1e96d153ac94a8fb93312
William Morriss <wjmelements@gmail.com>
2026-02-15 03:40:43 -0600

    define functionFacetPairs

--- src/interfaces/IERC8109Minimal.sol
+++ src/interfaces/IERC8109Minimal.sol
@@ -6,4 +6,11 @@ interface IERC8109Minimal {
     event SetDiamondFacet(bytes4 indexed selector, address indexed delegate);
 
     function facetAddress(bytes4 selector) external view returns (address);
+
+    struct FunctionFacetPair {
+        bytes4 selector;
+        address facet;
+    }
+
+    function functionFacetPairs() external view returns (FunctionFacetPair[] memory pairs);
 }
